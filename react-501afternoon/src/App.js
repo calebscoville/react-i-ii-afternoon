3df3601c204668750a8080244f6fc40d255d0025
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-01 16:51:21 -0600

    second

--- react-501afternoon/src/App.js
+++ react-501afternoon/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import './data.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello Friends</h1>
     </div>
   );
 }
